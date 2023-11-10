94f117555c17aff0cb5c3b9e1365be3ecdfc5df7
Nghiem Tuan Anh <nghiemtuananh2004@gmail.com>
2023-11-10 10:56:39 +0700

    add countdown timer

--- src/pages/MainGame/index.js
+++ src/pages/MainGame/index.js
@@ -1,6 +1,6 @@
 import { cardsImage } from "../../constant";
 import "./MainGame.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import SingleCard from "../../components/SingleCard";
 import Footer from "../../components/Footer";
 
@@ -8,12 +8,18 @@ export default function MainGame() {
     const [cards, setCards] = useState([]);
     const [choiceOne, setChoiceOne] = useState(null);
     const [choiceTwo, setChoiceTwo] = useState(null);
+    let [time, setTime] = useState(59);
+    let timeArray = String(time).split("").map((num) => Number(num))
+    const timeRef = useRef()
     // eslint-disable-next-line
     let [turn, setTurn] = useState(0);
     let [score, setScore] = useState(0);
     const [disabled, setDisable] = useState(false);
 
+    // Shuffle Card:
     const handleShuffle = () => {
+        setTime(59);
+        setDisable(false)
         const shuffledCards = [...cardsImage, ...cardsImage]
             .map(card => {
                 return {
@@ -50,6 +56,22 @@ export default function MainGame() {
         }
     }, [choiceOne, choiceTwo])
 
+    // Countdown Timer:
+    useEffect(() => {
+        timeRef.current = setInterval(() => {
+            setTime(prevTime => prevTime - 1)
+        }, 1000)
+        if (time <= 0) {
+            clearInterval(timeRef.current)
+            setDisable(true)
+        }
+        if (score === cards.length / 2) {
+            clearInterval(timeRef.current)
+            setDisable(true);
+        }
+        return () => clearInterval(timeRef.current);
+    }, [time])
+
     const handleClickCard = (cardItem) => {
         choiceOne ? setChoiceTwo(cardItem) : setChoiceOne(cardItem);
     }
@@ -69,16 +91,24 @@ export default function MainGame() {
                     {cards.length === 0 && <i className="note">*Click here to create new game</i>}
                     <button onClick={handleShuffle}>{cards.length > 0 ? "Restart" : "Create New Game"}</button>
                 </div>
-                <div className="grid">
-                    {cards.length > 0 && 
-                        cards.map(cardItem => 
-                                <SingleCard key={cardItem.id} cardItem={cardItem} handleClickCard={handleClickCard} flipped={cardItem === choiceOne || cardItem === choiceTwo || cardItem.matched} disabled={disabled}/>
-                        )}
-                </div>
                 {cards.length > 0 && 
-                    <div className="score">
-                        <span>Score: {score}</span>
+                <>
+                    <div className="grid">
+                        {cards.map(cardItem => 
+                                    <SingleCard key={cardItem.id} cardItem={cardItem} handleClickCard={handleClickCard} flipped={cardItem === choiceOne || cardItem === choiceTwo || cardItem.matched} disabled={disabled}/>
+                            )}
                     </div>
+                    <div className="time-score">
+                        <div className="time">
+                            <span>Time: 00:{timeArray.length === 2 ? `${time}` : `0${time}`}</span>
+                        </div>
+                        <div className="score">
+                            <span>Score: {score}</span>
+                        </div>
+                    </div>
+                    {time === 0 && <h1 className="time-out">TIME OUT!</h1>}
+                    {score === cards.length / 2 && <h1 className="you-win">YOU WIN!</h1>}
+                </>
                 }
             </div>
             <Footer />
